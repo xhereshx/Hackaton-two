213b101afd5f40d5409544f87cc5514733aa6c46
xhereshx <rasticamichal@email.cz>
2020-05-22 10:27:59 +0200

    packman

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,33 @@
 'use strict';
 
-console.log('it works!');
+
+let pacman = document.querySelector('.entity--pac'); 
+let xpos = 0;
+let left = 0;
+const TILE_SIZE = 85;
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowRight'){
+        xpos += TILE_SIZE;
+        left += TILE_SIZE;
+    }
+    console.log(pacman.style);
+    pacman.style.backgroundPositionX = `${xpos}px`;
+    pacman.style.left = `${left}px`;
+});
+
+
+
+class Pacman {
+    constructor(xpos,mouth){
+        this.xpos = xpos;
+        this.mouth = mouth;
+    }
+
+    moveRight(){
+    xpos += TILE_SIZE;
+    }
+
+    update(){
+        pacman.style.left = `${left}px`;
+    }
+}
